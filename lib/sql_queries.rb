3150d61335302c031bcc8c91b4494fe15614248e
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-26 06:45:29 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -14,7 +14,7 @@ end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "SELECT *, SUM(pledges.amount) FROM users
-JOIN users ON users.id = pledges.user_id
+INNER JOIN users ON users.id = pledges.user_id
 GROUP BY users.name ORDER BY users.name;"
 end
 
