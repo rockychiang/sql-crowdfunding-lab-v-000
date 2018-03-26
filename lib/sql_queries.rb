f0c9ce90bf9febf211809acbb4eb294b1a7857af
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-26 07:05:43 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,7 +31,7 @@ GROUP BY name ORDER BY SUM(pledges.amount), name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT category, SUM(pledges.amount) FROM projects
+"SELECT category, pledges.amount FROM projects
 JOIN pledges ON projects.id = pledges.project_id
 WHERE category = 'music';"
 end
