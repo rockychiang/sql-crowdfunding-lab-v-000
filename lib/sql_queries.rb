6eb83a3c63d3c6e5700bf3e677be579f7d3adbb0
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-26 06:18:09 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT projects.title SUM(pledges.amount) ORDER BY projects.title FROM projects
- INNER JOIN pledges ON project.id = pledge.project_id"
+ LEFT JOIN pledges ON project.id = pledge.project_id"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
